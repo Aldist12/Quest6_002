572dccc9df275f5509b2b378e75546347a8165e8
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-11-22 11:53:30 +0700

    Menambahkan elemen gambar logo dengan CircleShape

--- app/src/main/java/com/example/navigationwithdata/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationwithdata/ui/screen/RencanaStudyView.kt
@@ -1,22 +1,28 @@
 package com.example.navigationwithdata.ui.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.navigationwithdata.R
 import com.example.navigationwithdata.model.Mahasiswa
-import java.lang.reflect.Modifier
+
 
 @Composable
 fun RencanaStudyView(
@@ -39,12 +45,12 @@ fun RencanaStudyView(
     var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
 
     Column(
-        modifier = Modifier
+       modifier = androidx.compose.ui.Modifier
             .fillMaxSize()
             .background(color = colorResource(id = R.color.primary))
     ) {
         Row(
-            modifier = Modifier
+            modifier = androidx.compose.ui.Modifier
                 .fillMaxWidth()
                 .padding(16.dp),
             verticalAlignment = Alignment.CenterVertically
@@ -52,12 +58,12 @@ fun RencanaStudyView(
             Image(
                 painter = painterResource(id = R.drawable.logoumy),
                 contentDescription = "",
-                modifier = Modifier
+                modifier = androidx.compose.ui.Modifier
                     .clip(shape = CircleShape)
                     .size(100.dp)
             )
 
-        }
+
 
 
 
