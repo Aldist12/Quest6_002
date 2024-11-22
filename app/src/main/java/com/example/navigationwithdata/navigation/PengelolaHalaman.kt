9fe491b5357206c11d7c421bd55a82d6bef01bc7
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-11-22 12:46:39 +0700

    Menambahkan deklarasi fungsi MahasiswaApp dengan parameter ViewModel dan NavController

--- app/src/main/java/com/example/navigationwithdata/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/navigationwithdata/navigation/PengelolaHalaman.kt
@@ -1,8 +1,26 @@
 package com.example.navigationwithdata.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationwithdata.ui.view.viewmodel.MahasiswaViewModel
+import com.example.navigationwithdata.ui.view.viewmodel.RencanaStudyViewModel
+
 enum class PengelolaHalaman {
     Splash,
     Mahasiswa,
     Matakuliah,
     Tampil
 }
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
